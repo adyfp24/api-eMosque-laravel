de4c5f893979a880a383e1ba89e755ef8249d76b
adyfp24 <adyfp24@gmail.com>
2024-04-23 18:53:57 +0700

    feat: sanctum configuration

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/coba', function (){
+    return response()->json(['test' => 'test ini brow'], 200);
+});
