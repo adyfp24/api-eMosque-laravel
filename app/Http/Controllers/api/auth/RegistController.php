cc6250f1a65479fd18b67ef8a89eb91bc6747828
adyfp24 <adyfp24@gmail.com>
2024-04-25 22:18:02 +0700

    feat: register endpoint

---
+++ app/Http/Controllers/api/auth/RegistController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\api\auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegistController extends Controller
+{
+    public function register(Request $request){
+        $status = '';
+        $message = '';
+        $data = '';
+        $status_code = '';
+        try{
+            $newUser = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+                'role_id' => $request->role_id
+            ]);
+            if($newUser){
+                $message = 'registrasi telah berhasil dilakukan';
+                $status_code = 201;
+            }else{
+                $message = 'registrasi gagal';
+                $status_code = 400;
+            }
+            $status = 'success';
+            $data = $newUser;
+        }catch(\Exception $e){
+            $status = 'failed';
+            $message = 'Gagal menjalankan request'. $e->getMessage() ;
+            $status_code = 500;
+        }finally{
+            return response()->json([
+                'status' => $status,
+                'message' => $message,
+                'data' => $data
+            ], $status_code);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\auth\RegistController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +23,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/coba', function (){
     return response()->json(['test' => 'test ini brow'], 200);
 });
+
+Route::post('/register', [RegistController::class, 'register']);
